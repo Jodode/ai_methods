379b6c5ab2dc9bcceb8ba8044260c7abe1699d22
Jodode <tilrod.tv@gmail.com>
2024-10-08 02:24:00 +0300

    upload images

--- lab01/main.py
+++ lab01/main.py
@@ -1,23 +1,30 @@
 from requests import get, post, put, delete
 
 male_voice = [
-    'data/mp3/male_voice_message_1.mp3',
-    'data/mp3/male_voice_message_2.mp3',
-    'data/mp3/male_voice_message_3.mp3',
-    'data/mp3/male_voice_message_4.mp3',
-    'data/mp3/male_voice_message_5.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/male_voice_message_1.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/male_voice_message_2.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/male_voice_message_3.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/male_voice_message_4.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/male_voice_message_5.mp3',
 ]
 female_voice = [
-    'data/mp3/female_voice_message_1.mp3',
-    'data/mp3/female_voice_message_2.mp3',
-    'data/mp3/female_voice_message_3.mp3',
-    'data/mp3/female_voice_message_4.mp3',
-    'data/mp3/female_voice_message_5.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/female_voice_message_1.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/female_voice_message_2.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/female_voice_message_3.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/female_voice_message_4.mp3',
+    'https://github.com/Jodode/ai_methods/raw/refs/heads/main/lab01/data/mp3/female_voice_message_5.mp3',
  ]
 
 def get_voice_data(url):
-    response = get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    url = "https://open-ai21.p.rapidapi.com/texttoimage2"
+
+    payload = { "text": "dog" }
+    headers = {
+        "x-rapidapi-key": "c1ed10766amshcae26b62c408e44p1b56dejsn0318b221d1ff",
+        "x-rapidapi-host": "open-ai21.p.rapidapi.com",
+        "Content-Type": "application/json"
+    }
+
+    response = post(url, json=payload, headers=headers)
+
+    print(response.json())
